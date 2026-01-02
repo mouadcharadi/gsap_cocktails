88206bd651c9a9a39346261d54de3b79187c9a7a
Mouad charadi <moadchradi12@gmail.com>
2026-01-02 14:17:56 +0100

    Navbar & hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { ScrollTrigger , SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
 function App(props) {
     return (
-        <div className="flex-center h-dvh">
-            <h1 className="text-3xl text-indigo-300">Hello ,Mouad!</h1>
-        </div>
+        <main>
+          <Navbar/>
+          <Hero/>
+        </main>
     );
 }
 
